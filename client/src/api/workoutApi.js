ef859fa73f39b95a8328e438ae228a2dd24190cb
shawshank <shashanklakhaiyar71@gmail.com>
2026-08-29 13:41:01 +0530

    (feat):added update and delete apis

--- client/src/api/workoutApi.js
+++ client/src/api/workoutApi.js
@@ -13,7 +13,7 @@ export async function createWorkout(workout) {
     const response = await fetch(API_URL, {
         method: "POST",
         headers: {
-            "Content Type" : "application/json",
+            "Content-Type" : "application/json",
         },
         body : JSON.stringify(workout),
     });
--- server/server.js
+++ server/server.js
@@ -44,6 +44,51 @@ app.post("/api/workouts", async (req, res) => {
     }
 });
 
+app.put("/api/workouts/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const {name, date} = req.body;
+
+        const workout = await prisma.workout.update({
+            where: {
+                id: id,
+            },
+            data: {
+                name: name,
+                date: new Date(date),
+            },
+        });
+
+        res.json(workout);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Failed to upate workout",
+        });
+    }
+});
+
+app.delete("/api/workouts/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+
+        await prisma.workout.delete({
+            where: {
+                id: id,
+            },
+        });
+        
+        res.json({
+            message: "Workout deleted successfully",
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Failed to delete workout",
+        });
+    }
+});
+
 app.get("/api/test-db", async (req, res) => {
     try {
         const workouts = await prisma.workout.findMany();
